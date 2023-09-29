3c17da3f6fb92c1bbd76a3d101ccb9e8221f84f7
Vinayak_K_R <vinayakakr10@gmail.com>
2023-09-29 20:12:36 +0530

    commit

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -9,6 +9,7 @@ def call(String git_branch = 'main', String git_credentials = null, String git_u
         stages{
             stage ('Checkout code') {
                 steps {
+                    echo "This is dockerBuild.groovy"
                     checkout ([$class: 'GitSCM', 
                                branches: [[name: "*/${GIT_BRANCH}"]],   
                                userRemoteConfigs: [[credentialsId: "${GIT_CREDS}", 
